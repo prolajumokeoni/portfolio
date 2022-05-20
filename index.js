6fcce741c070d95dd54cf1d58afc0ce46fd84877
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2022-05-20 13:53:13 +0100

    Add icons and butttons

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const project = [
   {
 		image: '../assets/images/placeholder.png',
     title: "Crypto App",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See project",
   },
   {
 		image: '../assets/images/placeholder.png',
     title: "Metrics Web",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See project",
   },
   {
 		image: '../assets/images/placeholder.png',
     title: "Metrics Web",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See project",
   },
   {
 		image: '../assets/images/placeholder.png',
     title: "Metrics Web",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See Project",
   },
   {
 		image: '../assets/images/placeholder.png',
     title: "Metrics Web",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See project",
   },
   {
 		image: '../assets/images/placeholder.png',
     title: "Metrics Web",
-    button: "https://github.com/prolajumokeoni/metrics-web",
+    button: "See project",
   },
 ];
 
@@ -38,16 +38,19 @@ for (let i = 0; i < project.length; i++) {
   myProjects.innerHTML += `<div class="border m-4 rounded">
 	<img src="${project[i].image}">
 	<div class="text-center mb-3">
-	<h3>${project[i].title}</h3> 
+	<h3 class="t-3">${project[i].title}</h3> 
 	<div  class="d-flex">
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">React</small>
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Html</small>
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Css</small>
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Javascript</small>
-	</div>
-	
-	<button class="btn btn-outline-light green-background">See Project</button>
-	</div>
-	</div>
+	</div>  
+  <button class="myBtn">${project[i].button}</button> 
 	`;
 }
+
+
+ 
+
+
+ 
