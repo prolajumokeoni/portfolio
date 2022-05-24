bcd61e1e68ab602dbf47909ae19878bbafeba68b
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2022-05-24 19:25:20 +0100

    Update modal

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const project = [
     image: "./assets/images/cryptopopup.png",
     title: "Crypto App",
     framework: "React",
-    description: "Crypto app is an application where users can get information about the crypto market, exchange rates & news",
+    description:
+      "Crypto app is an application where users can get information about the crypto market, exchange rates & news",
     live: "https://6274fd2b09ac785e9138b4aa--prcryptoapp.netlify.app/",
     source: "https://github.com/prolajumokeoni/crypto-app",
   },
@@ -13,7 +14,8 @@ const project = [
     image: "./assets/images/metricspopup.png",
     title: "Metrics Web",
     framework: "React",
-    description: "Metrics-Web is a single page application and API based website that displays two pages. The Home page, where you can see, top 12 posts titles. And the Posts page, where you can get more details on a blog posts.",
+    description:
+      "Metrics-Web is a single page application and API based website that displays two pages. The Home page, where you can see, top 12 posts titles. And the Posts page, where you can get more details on a blog posts.",
     live: "https://6274fd2b09ac785e9138b4aa--prcryptoapp.netlify.app/",
     source: "https://github.com/prolajumokeoni/metrics-web",
   },
@@ -21,7 +23,8 @@ const project = [
     image: "./assets/images/penzpopup.png",
     title: "Penz Classes",
     framework: "React",
-    description: "An online class app that allows users to see all available courses, reserve a spot in a class, see all reservations, add and delete courses",
+    description:
+      "An online class app that allows users to see all available courses, reserve a spot in a class, see all reservations, add and delete courses",
     live: "https://penz-classes.netlify.app/",
     source: "https://github.com/prolajumokeoni/Penz_Classes_Frontend",
   },
@@ -39,7 +42,8 @@ const project = [
     title: "Bookstore",
     Framework: "See project",
     framework: "React",
-    description: "Bookstore website is built with react and redux, feature of the website includes:Display a list of books,Add a book and Remove a selected book.",
+    description:
+      "Bookstore website is built with react and redux, feature of the website includes:Display a list of books,Add a book and Remove a selected book.",
     live: "https://deploy-preview-4--serene-kare-79146f.netlify.app/",
     source: "https://github.com/prolajumokeoni/bookstore",
   },
@@ -66,7 +70,7 @@ for (let i = 0; i < project.length; i++) {
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Html</small>
 	<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Css</small> 
 	</div>  
-  <button onclick="openModal(project)" class="colorBtn mx-auto" type="button">See project</button>
+  <button onclick="openModal('${project[i].title}')" class="colorBtn mx-auto" type="button">See project</button>
   <div onclick="closeModal()" class="backdrop">
     <div class="modal" id="contentBody">
       
@@ -74,38 +78,31 @@ for (let i = 0; i < project.length; i++) {
   </div>`;
 }
 
-
 let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
- 
 
 function openModal(projectName) {
   let projectObj = null;
   project.forEach((obj) => {
-    if (obj.title == projectName)
-        projectObj = obj;
+    if (obj.title == projectName) projectObj = obj;
   });
 
+  let contentBody = `<span onclick="closeModal()" class="close p-2">&times;</span>
+  <img class="popupcard-img" src="${projectObj.image}">
+  <h3 class="pt-3">${projectObj.title}</h3> 
+  <div  class="d-flex text-center justify-content-center"> 
+  <small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">${projectObj.framework}</small>
+  <small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Javascript</small>
+  <small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Html</small>
+  <small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Css</small> 
+  </div>  
+    <p>${projectObj.description}</p>
+    <a class="colorBtn" href='${projectObj.live}' target="_blank">See live <img  src="../assets/images/popuparrow.png"></a>
+    <button class="colorBtn" href='${projectObj.source}';"> See source <img  src="../assets/images/gitpopup.png"></button>`;
 
-
-let contentBody = `<span onclick="closeModal()" class="close p-2">&times;</span>
-<img class="popupcard-img" src="${projectObj[i].image}">
-<h3 class="pt-3">${projectObj[i].title}</h3> 
-<div  class="d-flex text-center justify-content-center"> 
-<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">${projectObj[i].framework}</small>
-<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Javascript</small>
-<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Html</small>
-<small class="blue-font font-weight-bold bg-light m-3 p-2 rounded">Css</small> 
-</div>  
-  <p>${projectObj[i].description}</p>
-  <button class="colorBtn" onclick="window.location.href='${projectObj[i].live}';">See live <img  src="../assets/images/popuparrow.png"></button>
-  <button class="colorBtn" onclick="window.location.href='${projectObj[i].source}';"> See source <img  src="../assets/images/gitpopup.png"></button>
-`
-document.getElementById("contentBody").innerHTML = contentBody;
-backdrop.style.display = "block";
-modal.style.display = "block";
-
-
+  document.getElementById("contentBody").innerHTML = contentBody;
+  backdrop.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
