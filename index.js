9c3850426181405ad376f1d84229c2518d8a9164
prolajumokeoni <prolajumokeoni@gmail.com>
2022-12-13 12:19:03 +0100

    Update card

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-const projectCard = document.getElementById("project");
-
 const project = [
   {
-    image: "./assets/images/cryptopopup.png",
-    title: "Crypto App",
-    framework: "React",
+    image: './assets/images/musicplayer.png',
+    title: 'Music Player',
+    framework: 'React',
     description:
-      "Crypto app is an application where users can get information about the crypto market, exchange rates & news",
-    live: "https://6274fd2b09ac785e9138b4aa--prcryptoapp.netlify.app/",
-    source: "https://github.com/prolajumokeoni/crypto-app",
+      'Music player is a fullstack app with features like playlists, audio player, shuffle.',
+    live: 'https://github.com/prolajumokeoni/musicplayer.git',
+    source: 'https://github.com/prolajumokeoni/musicplayer.git',
   },
   {
-    image: "./assets/images/prometrics.png",
-    title: "Metrics Web",
-    framework: "React",
+    image: './assets/images/cryptopopup.png',
+    title: 'Crypto App',
+    framework: 'React',
     description:
-      "Metrics-Web is a single page application and API based website that displays two pages. The Home page, where you can see, top 12 posts titles. And the Posts page, where you can get more details on a blog posts.",
-    live: "https://prometrics.herokuapp.com/",
-    source: "https://github.com/prolajumokeoni/metrics-web",
+      'Crypto app is an application where users can get information about the crypto market, exchange rates & news',
+    live: 'https://6274fd2b09ac785e9138b4aa--prcryptoapp.netlify.app/',
+    source: 'https://github.com/prolajumokeoni/crypto-app',
   },
   {
-    image: "./assets/images/penzpopup.png",
-    title: "Penz Classes",
-    framework: "React",
+    image: './assets/images/penzpopup.png',
+    title: 'Penz Classes',
+    framework: 'React',
     description:
-      "An online class app that allows users to see all available courses, reserve a spot in a class, see all reservations, add and delete courses",
-    live: "https://penz-classes.netlify.app/",
-    source: "https://github.com/prolajumokeoni/Penz_Classes_Frontend",
+    'An online class app that allows users to see all available courses, reserve a spot in a class, see all reservations, add and delete courses',
+    live: 'https://penz-classes.netlify.app/',
+    source: 'https://github.com/prolajumokeoni/Penz_Classes_Frontend',
   },
   {
-    image: "./assets/images/htmlpopup.png",
-    title: "CCN Conference",
-    framework: "",
+    image: './assets/images/prometrics.png',
+    title: 'Metrics Web',
+    framework: 'React',
     description:
-      "Code Camp Nigeria is a web development course website, the project was built mainly for two screen sizes for mobile and desktop. This capstone project is developed to help implement all that has been learnt in the html-css module.",
-    live: "https://prolajumokeoni.github.io/html-css-capstone/",
-    source: "https://github.com/prolajumokeoni/html-css-capstone",
+      'Metrics-Web is a single page application and API based website that displays two pages. The Home page, where you can see, top 12 posts titles. And the Posts page, where you can get more details on a blog posts.',
+    live: 'https://prometrics.herokuapp.com/',
+    source: 'https://github.com/prolajumokeoni/metrics-web',
   },
   {
-    image: "./assets/images/bookpopup.png",
-    title: "Bookstore",
-    Framework: "See project",
-    framework: "React",
+    image: './assets/images/bookpopup.png',
+    title: 'Bookstore',
+    Framework: 'See project',
+    framework: 'React',
     description:
-      "Bookstore website is built with react and redux, feature of the website includes:Display a list of books,Add a book and Remove a selected book.",
-    live: "https://deploy-preview-4--serene-kare-79146f.netlify.app/",
-    source: "https://github.com/prolajumokeoni/bookstore",
+      'Bookstore website is built with react and redux, feature of the website includes:Display a list of books,Add a book and Remove a selected book.',
+    live: 'https://deploy-preview-4--serene-kare-79146f.netlify.app/',
+    source: 'https://github.com/prolajumokeoni/bookstore',
   },
   {
-    image: "./assets/images/mathpopup.png",
-    title: "Math Magicians",
-    framework: "React",
+    image: './assets/images/mathpopup.png',
+    title: 'Math Magicians',
+    framework: 'React',
     description:
-      "Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations, Read a random math-related quote. By building this application, I will learn how to use React.",
-    live: "https://pro-magic.herokuapp.com/",
-    source: "https://github.com/prolajumokeoni/math-magicians",
+      'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations, Read a random math-related quote. By building this application, I will learn how to use React.',
+    live: 'https://pro-magic.herokuapp.com/',
+    source: 'https://github.com/prolajumokeoni/math-magicians',
   },
 ];
-
-const myProjects = document.getElementById("myProjects");
+// disable eslint
+const myProjects = document.getElementById('myProjects');
 for (let i = 0; i < project.length; i++) {
   myProjects.innerHTML += `<div class="border m-4 rounded">
 	<img class="card-img" src="${project[i].image}">
@@ -78,16 +76,16 @@ for (let i = 0; i < project.length; i++) {
   </div>`;
 }
 
-let backdrop = document.querySelector(".backdrop");
-let modal = document.querySelector(".modal");
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
 
 function openModal(projectName) {
   let projectObj = null;
   project.forEach((obj) => {
-    if (obj.title == projectName) projectObj = obj;
+    if (obj.title === projectName) projectObj = obj;
   });
 
-  let contentBody = `<span onclick="closeModal()" class="close p-2">&times;</span>
+  const contentBody = `<span onclick="closeModal()" class="close p-2">&times;</span>
   <img class="popupcard-img" src="${projectObj.image}">
   <h3 class="pt-3">${projectObj.title}</h3> 
   <div  class="d-flex text-center justify-content-center"> 
@@ -100,12 +98,12 @@ function openModal(projectName) {
     <a class="colorBtn" href='${projectObj.live}' target="_blank">See live <img  src="../assets/images/popuparrow.png"></a>
     <button class="colorBtn" href='${projectObj.source}';"> See source <img  src="../assets/images/gitpopup.png"></button>`;
 
-  document.getElementById("contentBody").innerHTML = contentBody;
-  backdrop.style.display = "block";
-  modal.style.display = "block";
+  document.getElementById('contentBody').innerHTML = contentBody;
+  backdrop.style.display = 'block';
+  modal.style.display = 'block';
 }
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none;";
+  backdrop.style.display = 'none';
+  modal.style.display = 'none;';
 }
